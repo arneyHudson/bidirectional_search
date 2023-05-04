3d4f5e6bb4bab1ddba57629094ca40b004d47248
arneyh <arneyh@msoe.edu>
2023-05-04 13:51:29 -0500

    Made an initial Bidirectional search algorithm. May need testing.

--- UninformedSearch.py
+++ UninformedSearch.py
@@ -85,6 +85,54 @@ def bidirectional_search(problem: Problem) -> Any:
     """
     TODO
     """
+    initial_node = Node(problem.initial)
+    goal_node = Node(problem.goal)
+
+    if problem.is_goal(initial_node.state):
+        return get_path(initial_node)
+    if problem.is_goal(goal_node.state):
+        return get_path(goal_node)
+
+    # Initialize the frontiers and reached sets for the forward and backward searches
+    forward_frontier = [initial_node]
+    backward_frontier = [goal_node]
+    forward_reached = {problem.hashable_state(initial_node.state): initial_node}
+    backward_reached = {problem.hashable_state(goal_node.state): goal_node}
+
+    while forward_frontier and backward_frontier:
+        # Perform forward search expansion
+        forward_node = forward_frontier.pop()
+        for forward_child in problem.expand(forward_node):
+            forward_state = forward_child.state
+            forward_state_key = problem.hashable_state(forward_state)
+            if forward_state_key in backward_reached:
+                # Path found, combine paths from forward and backward searches
+                backward_node = backward_reached[forward_state_key]
+                forward_path = get_path(forward_child)
+                backward_path = get_path(backward_node)[::-1]  # Reverse the backward path
+                return forward_path + backward_path
+
+            if forward_state_key not in forward_reached:
+                forward_reached[forward_state_key] = forward_child
+                forward_frontier.append(forward_child)
+
+        # Perform backward search expansion
+        backward_node = backward_frontier.pop()
+        for backward_child in problem.expand(backward_node):
+            backward_state = backward_child.state
+            backward_state_key = problem.hashable_state(backward_state)
+            if backward_state_key in forward_reached:
+                # Path found, combine paths from forward and backward searches
+                forward_node = forward_reached[backward_state_key]
+                forward_path = get_path(forward_node)
+                backward_path = get_path(backward_child)[::-1]  # Reverse the backward path
+                return forward_path + backward_path
+
+            if backward_state_key not in backward_reached:
+                backward_reached[backward_state_key] = backward_child
+                backward_frontier.append(backward_child)
+
+    return []  # no path found
 
     return []  # no path found
 
